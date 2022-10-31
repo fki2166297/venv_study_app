8721e82a1e50c01028da5f60626d454692db7f55
fki2166297 <fki2166297@stu.o-hara.ac.jp>
2022-10-31 17:21:43 +0900

    10/31

--- study_app/accounts/models.py
+++ study_app/accounts/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    icon = models.ImageField(verbose_name='アイコン', upload_to='account_icons/', null=True, blank=True, default='')
+
     class Meta:
         verbose_name_plural = 'CustomUser'
